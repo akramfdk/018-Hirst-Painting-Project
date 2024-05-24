4121b828f846d3f82e1b0bca89de71214fbec5e7
akramfdk <akramjeet.singh@gmail.com>
2024-05-24 12:33:31 +1000

    final improvements - removing white dots, hiding turtle etc

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
-import colorgram
+# import colorgram
 import turtle as t
 import random
 
 hoppie = t.Turtle()
+t.colormode(255)
 
+hoppie.speed("fastest")
+hoppie.penup()
+hoppie.hideturtle()
 
-def extract_colors(num_colors):
-    """
-    extracts num_colors(int)
-    :param num_colors:
-    :return:
-    """
-    colors = colorgram.extract("hirst_image.jpg", num_colors)
-    rgb_colors_list = []
+# def extract_colors(num_colors):
+#     """
+#     extracts num_colors(int) number of colors from the image
+#     returns the list of rgb values for the chosen colors
+#     """
+#     colors = colorgram.extract("hirst_image.jpg", num_colors)
+#     rgb_colors_list = []
+#
+#     for color in colors:
+#         rgb_colors_list.append((color.rgb.r, color.rgb.g, color.rgb.b))
+#
+#     return rgb_colors_list
+#
+#
+# colors = extract_colors(25)
+# print(colors)
 
-    for color in colors:
-        rgb_colors_list.append((color.rgb.r, color.rgb.g, color.rgb.b))
-
-    return rgb_colors_list
-
-
-colors = extract_colors(10)
+colors = [(213, 154, 96), (52, 107, 132), (179, 77, 31), (202, 142, 31), (115, 155, 171), (124, 79, 99),
+          (122, 175, 156), (229, 236, 239), (226, 198, 131), (192, 87, 108), (11, 50, 64),
+          (55, 38, 19), (45, 168, 126), (47, 127, 123), (200, 121, 143), (168, 21, 29), (228, 92, 77),
+          (244, 162, 160), (38, 32, 35), (2, 25, 24), (78, 147, 171), (170, 23, 18)]
 
 
 def draw_dots(size, gap, steps):
-    t.colormode(255)
+    """
+    outputs the dots in a square of side (gap * steps)
+    the centre of screen coincides with the centre of square (provided steps are even)
+    """
 
     for count in range(steps):
-        hoppie.penup()
         x_pos = (-steps//2) * gap
         y_pos = (-steps//2 + count) * gap
         hoppie.setpos(x_pos, y_pos)
         hoppie.seth(0)
 
         for _ in range(steps):
-            hoppie.pendown()
             hoppie.dot(size, random.choice(colors))
-            hoppie.penup()
             hoppie.forward(gap)
 
 
-draw_dots(15, 40, 10)
+draw_dots(20, 50, 10)
 
 my_screen = t.Screen()
 my_screen.exitonclick()
